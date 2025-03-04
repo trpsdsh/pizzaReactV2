c015d8864e25883b545912e72a0c84b6db3c042a
trpsd <soldatenkoaleksander2@gmail.com>
2025-03-04 15:39:24 +0300

    #7 Skeleton test

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,16 @@
-import React, { act } from 'react';
+import React from 'react';
 function Categories() {
   const [activeIndex, setActiveIndex] = React.useState(0);
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
   return (
     <div className='categories'>
       <ul>
         {categories.map((value, i) => (
-          <li onClick={() => onClickCategory(i)} className={activeIndex === (i) ? 'active' : ''}>
+          <li
+            key={value}
+            onClick={() => setActiveIndex(i)}
+            className={activeIndex === i ? 'active' : ''}>
             {value}
           </li>
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logoSVG from '../assets/img/pizza-logo.svg'
+import logoSVG from '../assets/img/pizza-logo.svg';
 function Header() {
   return (
     <div className='header'>
