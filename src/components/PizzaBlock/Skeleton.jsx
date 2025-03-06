508c58b02fa32a62d89d3dff87837654279a0055
trpsd <soldatenkoaleksander2@gmail.com>
2025-03-06 08:11:34 +0300

    react

--- src/components/PizzaBlock/Skeleton.jsx
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const Skeleton = () => (
-  <ContentLoader
-    className='pizza__block'
+  <ContentLoader 
     speed={2}
     width={280}
     height={560}
-    viewBox='0 0 280 465'
-    backgroundColor='#f3f3f3'
-    foregroundColor='#ecebeb'>
-    <rect x='-1' y='313' rx='9' ry='9' width='280' height='90' />
-    <circle cx='147' cy='136' r='125' />
-    <rect x='5' y='270' rx='11' ry='11' width='280' height='30' />
-    <rect x='4' y='423' rx='9' ry='9' width='100' height='30' />
-    <rect x='114' y='417' rx='9' ry='9' width='160' height='45' />
+    viewBox="0 0 280 560"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <circle cx="130" cy="130" r="130" /> 
+    <rect x="0" y="270" rx="11" ry="11" width="280" height="30" /> 
+    <rect x="0" y="315" rx="8" ry="8" width="280" height="90" />
+    <rect x="3" y="435" rx="9" ry="9" width="100" height="27" /> 
+    <rect x="125" y="430" rx="9" ry="9" width="150" height="45" /> 
+
   </ContentLoader>
 );
 
