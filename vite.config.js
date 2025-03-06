b1d89929c7ebd081ac97fe663a5a6694c0745f17
trpsd <soldatenkoaleksander2@gmail.com>
2025-03-06 20:09:06 +0300

    test5
    Убраны предупреждения

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'color-functions', 'global-builtin'],
+        // Чтобы предупреждения с устаревшими функциями не светились
+      },
+    },
+  },
 });
